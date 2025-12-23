7d6c2f260850c722757ca4b2ba91dc0f071052ba
gabgustavo <gustavoavilabar@hotmail.com>
2025-12-22 20:47:40 -0500

    comentar el codigo

--- 02-tipos/04-methos-strings.py
+++ 02-tipos/04-methos-strings.py
@@ -1,13 +1,16 @@
 animanl = "  elefante   Elegante "
-print(animanl.upper())
-print(animanl.lower())
+print(animanl.upper())  # Convierte a mayusculas
+print(animanl.lower())  # convierte a minusculas
+# convertir la primera letra en mayuscula y elimina espacios al inicio y final
+# convertir la primera letra en mayuscula y elimina espacios al inicio y final
 print(animanl.strip().capitalize())
-print(animanl.title())
-print(animanl.strip())
-print(animanl.lstrip())
-print(animanl.rstrip())
-print(animanl.find('an'))
+print(animanl.title())  # convirte la primera letra de cada palabra en mayuscula
+print(animanl.strip())  # elimina espacios al inicio y final
+print(animanl.lstrip())  # elimina espacios al inicio
+print(animanl.rstrip())  # elimina espacios al final
+print(animanl.find('an'))  # busca la posicion de la primera ocurrencia de 'an'
+# busca la posicion de la primera ocurrencia de 'asd'
 print(animanl.find('asd'))
-print(animanl.replace('elefante', 'leon'))
+print(animanl.replace('elefante', 'leon'))  # reemplaza 'elefante' con 'leon'
 print('ante' in animanl)
 print('ante' not in animanl)
